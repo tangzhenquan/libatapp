4f29af0c8e6ceefa3fb2a21f459dbcd73adbf5b4
owent <owt5008137@live.com>
2016-07-18 12:30:36 +0800

    move

---
+++ include/include.macro.cmake
@@ -0,0 +1,6 @@
+﻿# =========== include - macro ===========
+set (PROJECT_ROOT_INC_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+include_directories("${PROJECT_ROOT_INC_DIR}")
+
+# define CONF from cmake to c macro
