1c8611e1d19ad3bb61b3c370d6c4190d23ec8e1c
owt5008137 <owt5008137@live.com>
2017-05-04 19:10:14 +0800

    use depth=1 when clone dep libs

--- 3rd_party/libatbus/libatbus.cmake
+++ 3rd_party/libatbus/libatbus.cmake
@@ -10,7 +10,7 @@ else()
     set (3RD_PARTY_LIBATBUS_PKG_DIR "${3RD_PARTY_LIBATBUS_BASE_DIR}/repo")
     if(NOT EXISTS ${3RD_PARTY_LIBATBUS_PKG_DIR})
         find_package(Git)
-        execute_process(COMMAND ${GIT_EXECUTABLE} clone "https://github.com/atframework/libATBUS.git" ${3RD_PARTY_LIBATBUS_PKG_DIR}
+        execute_process(COMMAND ${GIT_EXECUTABLE} clone --depth=1 "https://github.com/atframework/libatbus.git" ${3RD_PARTY_LIBATBUS_PKG_DIR}
             WORKING_DIRECTORY ${3RD_PARTY_LIBATBUS_BASE_DIR}
         )
     endif()
