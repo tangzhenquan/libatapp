78ef7c1952fffeb21a2a787badd890192d8befe9
owent <admin@owent.net>
2017-12-23 17:30:43 +0800

    移除atapp的日志check_interval

--- src/atframe/atapp_log_sink_maker.cpp
+++ src/atframe/atapp_log_sink_maker.cpp
@@ -8,22 +8,20 @@
 
 namespace atapp {
     namespace detail {
-        static util::log::log_wrapper::log_handler_t _log_sink_file(const std::string &sink_name, util::log::log_wrapper &logger,
-                                                                    uint32_t index, util::config::ini_value &ini_cfg) {
+        static util::log::log_wrapper::log_handler_t _log_sink_file(const std::string &sink_name, util::log::log_wrapper &logger, uint32_t index,
+                                                                    util::config::ini_value &ini_cfg) {
             std::string file_pattern = ini_cfg["file"].as_cpp_string();
             if (file_pattern.empty()) {
                 file_pattern = "server.%N.log";
             }
             size_t max_file_size = 0; // 64KB
             uint32_t rotate_size = 0; // 0-9
-            time_t check_interval = 0;
 
             util::log::log_sink_file_backend file_sink;
             if (ini_cfg.get_children().end() != ini_cfg.get_children().find("rotate")) {
                 util::config::ini_value &rotate_conf = ini_cfg["rotate"];
                 max_file_size = rotate_conf["size"].as<size_t>();
                 rotate_size = rotate_conf["number"].as_uint32();
-                check_interval = rotate_conf["interval"].as<time_t>();
             }
 
             if (0 == max_file_size) {
@@ -34,14 +32,9 @@ namespace atapp {
                 rotate_size = 10; // 0-9
             }
 
-            if (0 == check_interval) {
-                check_interval = 60; // 60s
-            }
-
             file_sink.set_file_pattern(file_pattern);
             file_sink.set_max_file_size(max_file_size);
             file_sink.set_rotate_size(rotate_size);
-            file_sink.set_check_interval(check_interval);
 
             if (ini_cfg.get_children().end() != ini_cfg.get_children().find("auto_flush")) {
                 uint32_t auto_flush = ini_cfg["auto_flush"].as_uint32();
@@ -56,8 +49,8 @@ namespace atapp {
             std::cout << std::endl;
         }
 
-        static util::log::log_wrapper::log_handler_t _log_sink_stdout(const std::string &sink_name, util::log::log_wrapper &logger,
-                                                                      uint32_t index, util::config::ini_value &ini_cfg) {
+        static util::log::log_wrapper::log_handler_t _log_sink_stdout(const std::string &sink_name, util::log::log_wrapper &logger, uint32_t index,
+                                                                      util::config::ini_value &ini_cfg) {
             return _log_sink_stdout_handle;
         }
 
@@ -66,11 +59,11 @@ namespace atapp {
             ss() << util::cli::shell_font_style::SHELL_FONT_COLOR_RED << content << std::endl;
         }
 
-        static util::log::log_wrapper::log_handler_t _log_sink_stderr(const std::string &sink_name, util::log::log_wrapper &logger,
-                                                                      uint32_t index, util::config::ini_value &ini_cfg) {
+        static util::log::log_wrapper::log_handler_t _log_sink_stderr(const std::string &sink_name, util::log::log_wrapper &logger, uint32_t index,
+                                                                      util::config::ini_value &ini_cfg) {
             return _log_sink_stderr_handle;
         }
-    }
+    } // namespace detail
 
     log_sink_maker::log_sink_maker() {}
 
@@ -97,4 +90,4 @@ namespace atapp {
     }
 
     log_sink_maker::log_reg_t log_sink_maker::get_stderr_sink_reg() { return detail::_log_sink_stderr; }
-}
+} // namespace atapp
