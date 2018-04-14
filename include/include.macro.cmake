e7b0158a6a43545412711877748b24650b79b65a
owent <admin@owent.net>
2018-04-14 17:02:49 +0800

    优化复用的变量

--- include/include.macro.cmake
+++ include/include.macro.cmake
@@ -1,6 +1,6 @@
 ﻿# =========== include - macro ===========
-set (PROJECT_ROOT_INC_DIR ${CMAKE_CURRENT_LIST_DIR})
+set (PROJECT_LIBATAPP_ROOT_INC_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-include_directories("${PROJECT_ROOT_INC_DIR}")
+include_directories(${PROJECT_LIBATAPP_ROOT_INC_DIR})
 
 # define CONF from cmake to c macro
